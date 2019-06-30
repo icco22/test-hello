8423424c1dcceeb2e0752bd45e0ffe494298a206
icco22 <hodzic22@gmail.com>
2019-06-30 17:26:58 +0200

    Add files via upload

---
+++ src/main/java/com/juso/ba/test_proba/App.java
@@ -0,0 +1,13 @@
+package com.juso.ba.test_proba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/juso/ba/test_proba/HomeTest.java
@@ -0,0 +1,26 @@
+package com.juso.ba.test_proba;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\HP EliteBook 8560W\\Desktop\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "http://localhost:9095//helloWorld";
+		driver.get(baseUrl);
+		Thread.sleep(3000);
+		WebElement helloWorld = driver.findElement(By.xpath("//h2[contains(text(),'Hello World!')]"));
+		if(helloWorld.isDisplayed()) {
+			System.out.println("Test prosao");
+		} else {
+			System.out.println("Test pao");
+		}
+		driver.quit();
+	}
+
+}
